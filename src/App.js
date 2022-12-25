3c8440a9046d3aee77d28d7c847427886aba0519
user <ujjalroy7862@gmail.com>
2022-12-25 21:04:51 +0600

    start the new project which based on map rounting

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState, useRef } from "react";
+import "@tomtom-international/web-sdk-maps/dist/maps.css";
+import * as tt from "@tomtom-international/web-sdk-maps";
+
+import "./App.css";
+
+function App() {
+  const mapElement = useRef();
+  const [map, setMap] = useState({});
+  const [longitude, setLongitude] = useState(-0.112869);
+  const [latitude, setLatitude] = useState(51.504);
+  useEffect(() => {
+    let map = tt.map({
+      key: process.env.REACT_APP_TOM,
+      container: mapElement.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true,
+      },
+      center: [longitude, latitude],
+      zoom: 14,
+    });
+
+    setMap(map);
+
+    const addMarker = () => {
+      const element = document.createElement("div");
+      element.className = "marker";
+      const marker = new tt.Marker({
+        draggable: true,
+        element: element,
+      })
+        .setLngLat([longitude, latitude])
+        .addTo(map);
+
+      marker.on("dragend", () => {
+        const lnglat = marker.getLngLat();
+        setLongitude(lnglat.lng);
+        setLatitude(lnglat.lat);
+      });
+    };
+
+    addMarker();
+
+    return () => map.remove();
+  }, [longitude, latitude]);
+
+  return (
+    <>
+      {map && (
+        <div className="app">
+          <div ref={mapElement} className="map" />
+          <div className="search-bar">
+            <h1>Where to???</h1>
+            <input
+              type="text"
+              id="longitude"
+              className="longitude"
+              placeholder="Put in Longitude"
+              onChange={(e) => setLongitude(e.target.value)}
+            />
+            <input
+              type="text"
+              id="latitude"
+              className="latitude"
+              placeholder="Put in Latitude"
+              onChange={(e) => setLatitude(e.target.value)}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
